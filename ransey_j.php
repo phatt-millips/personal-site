c0da68c62690ecb136ba5190c54a2c5360f232be
Matt Phillips <matt.phillips96@gmail.com>
2017-05-17 15:14:26 -0500

    Most of the HTML laid out. CSS, PHP, and JS still need work.

---
+++ ransey_j.php
@@ -0,0 +1,83 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<link href="personal-site.css" type="text/css" rel="stylesheet" />
+		<meta charset="UTF-8" />
+		<title></title>
+	</head>
+	<body>
+	<div class = "title_page">	
+		<div class = "logo">
+			<a href = ""><img src = "" title = "" alt = ""/></a>
+		</div>
+		<div class = "navigation">
+			<a href = "">Home</a>
+			<a href = "">Work</a>
+			<a href = "">Mission</a>
+			<a href = "">About</a>
+			<a href = "">Contact</a>
+		</div>
+		<p class = "mission_statement">
+		I help...
+		</p>
+		<div class = "nav_arrow">
+			<img src = ""/>	
+		</div>
+	</div>
+	<div class = "work">
+		<div class = "title">
+			<h2>My Work</h2>
+		</div>
+		<div class = "navigation">
+			<!-- populated by php -->
+		</div>
+		<div class = "products"> 
+			<div class = "<?= ?>">
+				<!-- populated by php -->
+			</div>
+		</div>	
+	</div>
+	<div class = "mission">
+		<h2>
+			What I do
+		</h2>
+		<p>
+			Yada
+		</p>
+		<p>
+			Yada
+		</p>
+		<p>
+			Yada
+		</p>
+
+	</div>
+	<div class = "about">
+		<h2>
+			About me
+		</h2>
+		<h3> 
+			<strong>Contact</strong><br/>
+			<a href = "">inquire..</a><br/>
+			phone numb	
+		</h3>
+		<p>
+		
+		</p>
+		<p>
+
+		</p>
+		<p>
+
+		</p>
+		<img src = ""/>
+	</div>
+	<div class = "contact">
+		<div class = "links">
+			
+		</div>
+	</div>
+	
+	</body>
+</html>
